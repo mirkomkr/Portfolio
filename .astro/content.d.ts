bf47a2a1168de1aeca8bf570db4f85ac35a46915
Mirko Passeri <bionikle746@gmal.com>
2026-02-24 22:10:40 +0100

    feat: Add new project images and update the project array with detailed project information.

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -168,7 +168,7 @@ declare module 'astro:content' {
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
 	type ExtractLoaderTypes<T> = T extends import('astro/loaders').LiveLoader<
-		infer TData,
+		infer TData extends Record<string, any>,
 		infer TEntryFilter,
 		infer TCollectionFilter,
 		infer TError
--- src/components/projectArray.js
+++ src/components/projectArray.js
@@ -1,23 +1,51 @@
+import LittleLemon from "../assets/progetti/Little-Lemon-Meta-FrontEnd.webp";
+import AppMeteo from "../assets/progetti/app-metep-clima-e-mare.webp";
+import DtfItalia from "../assets/progetti/dtf-italia.webp";
+import ToDoListReact from "../assets/progetti/to-do-list-react.webp";
+import ToDoListVanillaJS from "../assets/progetti/to-do-list-vanilla-JS.webp";
+import GeorgeRomero from "../assets/progetti/george-romero-tribute-page.webp";
+
 export const projects = [
   {
-    title: "Progetto 1",
-    description: "Descrizione del progetto 1",
-    image: "https://via.placeholder.com/150",
+    title: "DTF Italia",
+    description: "Sito e-commerce per la vendita di prodotti DTF",
+    image: "DtfItalia",
     altimage: "Immagine del progetto 1",
-    link: "https://www.google.com",
+    link: "https://www.dtfitalia.it/",
   },
   {
-    title: "Progetto 2",
+    title: "App Meteo",
     description: "Descrizione del progetto 2",
-    image: "https://via.placeholder.com/150",
+    image: "AppMeteo",
     altimage: "Immagine del progetto 2",
-    link: "https://www.google.com",
+    link: "https://meteo-clima-e-mare.vercel.app/",
   },
   {
-    title: "Progetto 3",
+    title: "To Do List React",
     description: "Descrizione del progetto 3",
-    image: "https://via.placeholder.com/150",
+    image: "ToDoListReact",
     altimage: "Immagine del progetto 3",
-    link: "https://www.google.com",
+    link: "https://react-todo-app-flax-omega.vercel.app/",
+  },
+  {
+    title: "Little Lemon",
+    description: "Descrizione del progetto 4",
+    image: "LittleLemon",
+    altimage: "Immagine del progetto 4",
+    link: "https://little-lemon-capstone-meta-frontend.vercel.app/",
+  },
+  {
+    title: "To Do List Vanilla JS",
+    description: "Descrizione del progetto 5",
+    image: "ToDoListVanillaJS",
+    altimage: "Immagine del progetto 5",
+    link: "https://codepen.io/mirkomkr/full/PwYBxOo",
+  },
+  {
+    title: "George A. Romero Tribute Page",
+    description: "Descrizione del progetto 6",
+    image: "GeorgeRomero",
+    altimage: "Immagine del progetto 6",
+    link: "https://codepen.io/mirkomkr/full/xxvLqBJ",
   },
 ];
