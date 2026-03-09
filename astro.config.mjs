5f29a7e844f67e150c29f31f0fc191ab2d63236e
Mirko Passeri <bionikle746@gmal.com>
2026-03-09 08:48:07 +0100

    feat: integrate sitemap plugin into Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mirkopasseri.it",
