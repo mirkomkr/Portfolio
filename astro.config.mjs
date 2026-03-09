66604f0688bef19a4c55a6d62237a5947b1300af
Mirko Passeri <bionikle746@gmal.com>
2026-03-09 09:07:49 +0100

    feat: Add `astro-robots-txt` integration for generating `robots.txt`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mirkopasseri.it",
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(), robotsTxt()],
 
   vite: {
     plugins: [tailwindcss()],
