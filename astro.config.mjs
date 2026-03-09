a4b65cd6c7125f4a24c14be5c3ba6b144a56989e
Mirko Passeri <bionikle746@gmal.com>
2026-03-09 08:47:31 +0100

    feat: Implement sitemap, enhance SEO with updated Schema.org data, and improve accessibility attributes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.mirkopasseri.it",
   integrations: [icon()],
 
   vite: {
